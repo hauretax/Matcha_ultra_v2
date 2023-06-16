ded527a858e33cea02761cb00d6d66023357792c
hauretax <huggotrico@protonmail.ch>
2023-06-16 16:05:24 +0200

    s

---
+++ back_src/controllers/JwtCtrl.ts
@@ -0,0 +1,17 @@
+
+//cree une entiter pour stocker les jwt .
+
+//implementer une classe avec
+//set jwt cree un jwt avec le tokene envoyer le stocke dans la list des token avec la cle de l'id utilisateur.
+//set jwRefreshToken avec le token envoyer et stocker dans la list des refreshtoken avec la cle id user
+//verified jwt
+//verified jwrefreshtoken premet d'envoyer un id usr et un token et en verifie la validiter
+//les function liee q jwrefreshtoken peuvent rester privee
+//
+//implementer la verificqtion des token (verifier dans la liste)
+
+
+
+
+
+// let JwtStore: Array<>
