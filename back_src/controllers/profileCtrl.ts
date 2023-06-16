9db140ab83e914793b08913871915d12ecf56708
Antoine Labalette <labalette.antoine@gmail.com>
2023-06-17 00:47:27 +0200

    Update login controller check if body login becomes username add jwt remove useless try catch because of global error handling

--- back_src/controllers/profileCtrl.ts
+++ back_src/controllers/profileCtrl.ts
@@ -1,63 +1,68 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import UserDb from "../database/User.db";
 import sendEmail from "../utils/sendMail";
 import bcrypt from "bcrypt";
-import {  UserReqRegister } from "../../comon_src/type/user.type";
+import { UserReqRegister } from "../../comon_src/type/user.type";
 import { checkDataProfilCreate } from "./dataVerifiers/assertedUserData";
+import { UserPayload } from "../../comon_src/type/user.type"
+import { generateJwt } from "../utils/jwt"
 
 const userDB = new UserDb;
 
-export async function  createProfile(req: Request, res: Response) {
-	const profile: UserReqRegister = req.body;
-	const dataError = checkDataProfilCreate(profile);
-	if (dataError) {
-		res.status(dataError.code).json({ error: dataError.message });
-	}
-	try {
-		profile.password = await bcrypt.hash(req.body.password, 10);
-		const { id, accessCode, email } = await userDB.insertUser(profile);
-		//TODO: faire un lien en front pour pouvoir verifier le mail (url est pas bon)
-		sendEmail(email, "click on this link to activate account :http://" + "localhost:" + "8080/" + accessCode);
-		res.status(201).json({ message: "Profile created", usrId: id });
+export async function createProfile(req: Request, res: Response) {
+  const profile: UserReqRegister = req.body;
+  const dataError = checkDataProfilCreate(profile);
+  if (dataError) {
+    return res.status(dataError.code).json({ error: dataError.message });
+  }
+  try {
+    profile.password = await bcrypt.hash(req.body.password, 10);
+    const { id, accessCode, email } = await userDB.insertUser(profile);
+    //TODO: faire un lien en front pour pouvoir verifier le mail (url est pas bon)
+    sendEmail(email, "click on this link to activate account :http://" + "localhost:" + "8080/" + accessCode);
+    res.status(201).json({ message: "Profile created", usrId: id });
 
-	} catch (error) {
-		if (error === 409) {
-			res.status(409).json({ error: "user or email already taken" });
-			return;
-		}
-		console.log("\nregister_error:\n", error);
-		res.status(500).json({ error: "Internal Server Error" });
-		return;
-	}
+  } catch (error) {
+    if (error === 409) {
+      res.status(409).json({ error: "user or email already taken" });
+      return;
+    }
+    console.log("\nregister_error:\n", error);
+    res.status(500).json({ error: "Internal Server Error" });
+    return;
+  }
 }
 
-export async function login(req: Request, res: Response) {
-	const login: string = req.body.username || req.body.email;
-	const password = req.body.password;
-	if (!login || !password) {
-		res.status(422).json({ error: "Unprocessable Entity" });
-		return;
-	}
-	try {
-		const fulluser = await userDB.findUser(login);
-		if (fulluser === null) {
-			res.status(404).json({ error: "Not Found" });
-			return;
-		}
-		const isAutorized = await bcrypt.compare(password, fulluser.password);
-		if (isAutorized) {
-			//TODO add jsonwebtoken
-			const { email, username, firstName, lastName, emailVerified } = fulluser;
-			res.status(200).json({
-				user: { email, username, lastName, emailVerified, firstName }
-			});
-		} else {
-			res.status(401).json({ error: "Unauthorized" });
-		}
-	} catch (error) {
-		console.log("\nlogin_error:\n", error);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+export async function login(req: Request, res: Response, next: NextFunction) {
+  if (!req.body) return res.status(400).json({ error: "no credentials provided" })
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(422).json({ error: "username and/or password missing" });
+  }
+
+  const fulluser = await userDB.findUser(username);
+  if (fulluser === null) {
+    return res.status(404).json({ error: "account not found" });
+  }
+  const isAutorized = await bcrypt.compare(password, fulluser.password);
+  if (isAutorized) {
+    //TODO add jsonwebtoken
+    const { id, email, username, firstName, lastName, emailVerified } = fulluser;
+    const payload: UserPayload = {
+      jwtToken: generateJwt(id),
+      profile: {
+        email,
+        username,
+        lastName,
+        firstName,
+        emailVerified
+      }
+    };
+    res.status(200).json(payload);
+  } else {
+    res.status(401).json({ error: "username and/or password incorrect" });
+  }
+
 }
 
 
