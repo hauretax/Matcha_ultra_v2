a180a2149ed42508d1daab34cb6ed457e491b936
Antoine Labalette <labalette.antoine@gmail.com>
2023-06-17 01:24:23 +0200

    Implement async handler on register controller

--- back_src/routes/profileRoutes.ts
+++ back_src/routes/profileRoutes.ts
@@ -4,10 +4,7 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.post("/register", (req: Request, res: Response) => {
-	console.log("call profile create");
-	createProfile(req, res);
-});
+router.post("/register", asyncHandler(createProfile));
     
 router.post("/login", asyncHandler(login))
 
