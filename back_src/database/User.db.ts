121bd09410a2d2332bd2ac489df173caeb50b17c
Antoine Labalette <labalette.antoine@gmail.com>
2023-06-17 01:34:49 +0200

    Refactor

--- back_src/database/User.db.ts
+++ back_src/database/User.db.ts
@@ -1,76 +1,78 @@
 import { FullUser, UserReqRegister } from "../../comon_src/type/user.type";
 import Dbhandler from "./DbHandler";
-interface test {
+
+interface InsertedUser {
   id: number,
   email: string,
   accessCode: number
 }
+
 export default class UserDb extends Dbhandler {
-	//TODO trouver un nom a test
-	async insertUser(user: UserReqRegister): Promise<test> {
-		//peu etre plus de sens de le mettre dans profileCtrl ?
-		const accessCode = Math.floor(Math.random() * 90000 + 10000);
-		const query = `
+  async insertUser(user: UserReqRegister): Promise<InsertedUser> {
+    //peu etre plus de sens de le mettre dans profileCtrl ?
+    const accessCode = Math.floor(Math.random() * 90000 + 10000);
+    const query = `
             INSERT INTO users (email, username, lastName, firstName, password, accessCode, emailVerified)
             VALUES (?, ?, ?, ?, ?, ?, ?)
           `;
-		const params = [user.email, user.username, user.lastName, user.firstName, user.password, accessCode, 0];
-		//TODO rename test
-		return new Promise<test>((resolve, reject) => {
-			this.db.run(query, params, function (err) {
-				if (err) {
-					if (err.message.includes("UNIQUE constraint failed")) {
-						reject(409);
-						return;
-					} else {
-						console.log(err);
-						reject(500);
-						return;
+    const params = [user.email, user.username, user.lastName, user.firstName, user.password, accessCode, 0];
+    return new Promise<InsertedUser>((resolve, reject) => {
+      this.db.run(query, params, function (err) {
+        if (err) {
+          if (err.message.includes("UNIQUE constraint failed")) {
+            reject(409);
+            return;
+          } else {
+            console.log(err);
+            reject(500);
+            return;
 
-					}
-				} else {
-					resolve({ id: this.lastID, accessCode, email: user.email });
-				}
-			});
-		});
-	}
+          }
+        } else {
+          resolve({ id: this.lastID, accessCode, email: user.email });
+        }
+      });
+    });
+  }
 
-	deleteUser(id) {
-		try {
-			this.db.run("DELETE FROM users WHERE id = ?", [id]);
-		} catch (err) {
-			console.log("deleteUser say:", err);
-		}
-	}
+  deleteUser(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.db.run("DELETE FROM users WHERE id = ?", [id], (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+  }
 
 
-	findUser(login: string): Promise<FullUser | null> {
-		const query = `
+  findUser(login: string): Promise<FullUser | null> {
+    const query = `
         SELECT *
         FROM users
         WHERE username = ?
       `;
 
-		return new Promise((resolve, reject) => {
-			this.db.all(query,
-				[login],
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				(err: Error, rows: any[]) => {
-					if (err) {
-						reject(err);
-						return;
-					}
-					//4 eme niveaux de complexiter on s en soucie ?
-					if (rows && rows.length > 0) {
-						const user: FullUser = rows[0];
-						resolve(user);
-					} else {
-						resolve(null);
-					}
-				}
-			);
+    return new Promise((resolve, reject) => {
+      this.db.all(query, [login], (err: Error, rows: FullUser[]) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          //4 eme niveaux de complexiter on s en soucie ?
+          if (rows && rows.length > 0) {
+            const user: FullUser = rows[0];
+            resolve(user);
+          } else {
+            resolve(null);
+          }
+        }
+      );
 
-		});
-	}
+    });
+  }
 
 }
