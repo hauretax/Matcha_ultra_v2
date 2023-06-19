c8e328831da7bf6fc9a7ee119059b10059797f30
huggo trico <huggotricot@protonmail.ch>
2023-06-19 18:20:46 +0200

    rename test

--- back_src/database/User.db.ts
+++ back_src/database/User.db.ts
@@ -1,13 +1,12 @@
 import { FullUser, UserReqRegister } from "../../comon_src/type/user.type";
 import Dbhandler from "./DbHandler";
-interface test {
+interface UserCredentials  {
   id: number,
   email: string,
   accessCode: number
 }
 export default class UserDb extends Dbhandler {
-	//TODO trouver un nom a test
-	async insertUser(user: UserReqRegister): Promise<test> {
+	async insertUser(user: UserReqRegister): Promise<UserCredentials > {
 		//peu etre plus de sens de le mettre dans profileCtrl ?
 		const accessCode = Math.floor(Math.random() * 90000 + 10000);
 		const query = `
@@ -15,8 +14,7 @@ export default class UserDb extends Dbhandler {
             VALUES (?, ?, ?, ?, ?, ?, ?)
           `;
 		const params = [user.email, user.username, user.lastName, user.firstName, user.password, accessCode, 0];
-		//TODO rename test
-		return new Promise<test>((resolve, reject) => {
+		return new Promise<UserCredentials>((resolve, reject) => {
 			this.db.run(query, params, function (err) {
 				if (err) {
 					if (err.message.includes("UNIQUE constraint failed")) {
