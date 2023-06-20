a84f559552eb7a0c11e77334f65ff2484f0be5a4
Antoine Labalette <labalette.antoine@gmail.com>
2023-06-21 00:07:50 +0200

    Update architecture for db handling create library to simplify queries create User object to group methods that interacts with User table create errors class update profile controller update db initialization in app.tsx

---
+++ back_src/database/errors.ts
@@ -0,0 +1,14 @@
+// errors.ts
+export class UniqueConstraintError extends Error {
+  constructor(message?: string) {
+      super(message);
+      this.name = 'UniqueConstraintError';
+  }
+}
+
+export class DatabaseError extends Error {
+  constructor(message?: string) {
+      super(message);
+      this.name = 'DatabaseError';
+  }
+}
