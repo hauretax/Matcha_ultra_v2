41c3595229e5402a7b3c43a5fa947c3eb4e86a05
hauretax <huggotrico@protonmail.ch>
2023-06-28 15:41:39 +0200

    i did it

--- back_src/middlewares/secureRequest.mid.ts
+++ back_src/middlewares/secureRequest.mid.ts
@@ -44,28 +44,3 @@ export async function validsecurRequest(
 	}
 
 }
-
-export async function isPictureOwner(req: Request, res: Response, next: NextFunction) {
-	const user: UserProfile = res.locals.fulluser; // from validsecurRequest
-	const { pictureId } = req.params;
-	if (!pictureId || !Number.isInteger(parseInt(pictureId))) {
-		res.status(400).json({ error: "invalid picture id" });
-		return;
-	}
-	const picture = user.pictures.find((picture) => picture.id === parseInt(pictureId));
-	if (!picture) {
-		res.status(404).json({ error: "picture not found" });
-		return;
-	}
-	res.locals.picture = picture;
-	next();
-}
-
-export async function isEmailVerified(req: Request, res: Response, next: NextFunction) {
-	const user: UserProfile = res.locals.fulluser; // from validsecurRequest
-	if (!user.emailVerified) {
-		res.status(422).json({ error: "unverified email" });
-		return;
-	}
-	next();
-}
