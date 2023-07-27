32a128305c85e28c3a6b0daa4128198584e82220
Antoine Labalette <labalette.antoine@gmail.com>
2023-07-27 15:13:53 +0200

    axiosPrivate.ts: fix dumb mistakes

--- client/src/services/axiosPrivate.ts
+++ client/src/services/axiosPrivate.ts
@@ -21,16 +21,16 @@ axiosInstance.interceptors.response.use(
   async function (error) {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response.status === 403 && !originalRequest._retry) {
 
       originalRequest._retry = true;
       
-      const response = await axios.post('/api/refresh', {
+      const response = await axiosInstance.post('/newToken', {
         'refreshToken': localStorage.getItem('refreshToken'),
       });
 
-      if (response.status === 201) {
-        localStorage.setItem('accessToken', response.data.accessToken);
+      if (response.status === 200) {
+        localStorage.setItem('accessToken', response.data.token);
         localStorage.setItem('refreshToken', response.data.refreshToken);
         return axiosInstance(originalRequest);
       }
