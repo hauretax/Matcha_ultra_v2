e703a2ba17b846c6fb47610affaff5027b8c12b3
Antoine Labalette <labalette.antoine@gmail.com>
2023-07-31 14:25:05 +0200

    security : introduce permission groups to not make a mistake by inadvertance add isProfileCompleted middleware

--- back_src/routes/profileRoutes.ts
+++ back_src/routes/profileRoutes.ts
@@ -3,26 +3,34 @@ import { createProfile, getProfile, getOptions, updateProfile, updateInterests, 
 import asyncHandler from "express-async-handler";
 import { createNewJwt } from "../controllers/jwtCtrl";
 import { validsecurRequest } from "../middlewares/secureRequest.mid";
-import { isEmailVerified, isPictureOwner } from "../middlewares/protectRequest.mid";
+import { isProfileCompleted, isPictureOwner } from "../middlewares/protectRequest.mid";
 import upload from "../config/multer.config";
 import setUserPosition from "../controllers/localisationCtrl";
 const router = express.Router();
 
-router.post("/register", asyncHandler(createProfile));
-router.post("/login", asyncHandler(login));
-router.post("/setLocalisation", validsecurRequest, asyncHandler(setUserPosition));
-router.post("/newToken", asyncHandler(createNewJwt));
-router.get("/profile", validsecurRequest, asyncHandler(getProfile));
-router.get("/options", validsecurRequest, asyncHandler(getOptions));
-router.patch("/profile", validsecurRequest, asyncHandler(updateProfile));
-router.patch("/profileBio", validsecurRequest, asyncHandler(updateBio));
-router.patch("/profileInterests", validsecurRequest, asyncHandler(updateInterests));
-router.post("/picture/new", validsecurRequest, upload.single("file"), asyncHandler(insertPicture));
-router.put("/picture/:pictureId/edit", validsecurRequest, isPictureOwner, upload.single("file"), asyncHandler(updatePicture));
-router.delete("/picture/:pictureId", validsecurRequest, asyncHandler(deletePicture));
-router.get("/verify_email", asyncHandler(validByEmail));
-router.post("/request_password_reset", asyncHandler(RequestpasswordReset));
-router.post("/reset_password", asyncHandler(passwordReset));
-router.get("/users", validsecurRequest, isEmailVerified, asyncHandler(getProfiles));
+const publicGroup = [];
+const privateGroup = [validsecurRequest];
+const pictureOwnerGroup = [validsecurRequest, isPictureOwner];
+const profileCompletedGroup = [validsecurRequest, isProfileCompleted];
+
+router.post("/register", publicGroup, asyncHandler(createProfile));
+router.post("/login", publicGroup, asyncHandler(login));
+router.get("/verify_email", publicGroup, asyncHandler(validByEmail));
+router.post("/request_password_reset", publicGroup, asyncHandler(RequestpasswordReset));
+router.post("/reset_password", publicGroup, asyncHandler(passwordReset));
+router.post("/newToken", publicGroup, asyncHandler(createNewJwt));
+
+router.post("/setLocalisation", privateGroup, asyncHandler(setUserPosition));
+router.get("/profile", privateGroup, asyncHandler(getProfile));
+router.get("/options", privateGroup, asyncHandler(getOptions));
+router.patch("/profile", privateGroup, asyncHandler(updateProfile));
+router.patch("/profileBio", privateGroup, asyncHandler(updateBio));
+router.patch("/profileInterests", privateGroup, asyncHandler(updateInterests));
+router.post("/picture/new", privateGroup, upload.single("file"), asyncHandler(insertPicture));
+
+router.put("/picture/:pictureId/edit", pictureOwnerGroup, upload.single("file"), asyncHandler(updatePicture));
+router.delete("/picture/:pictureId", pictureOwnerGroup, asyncHandler(deletePicture));
+
+router.get("/users", validsecurRequest, profileCompletedGroup, asyncHandler(getProfiles));
 
 export default router;  
