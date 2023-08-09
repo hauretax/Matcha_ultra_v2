c9718dc3856f2f7ffca069ea56d6c06108c72322
Huggo <31890474+hauretax@users.noreply.github.com>
2023-08-09 19:42:34 +0200

    notification

--- back_src/controllers/notificationCtrl.ts
+++ back_src/controllers/notificationCtrl.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import InsertDb from "../database/Insert.db";
 import { notification, notificationType } from "../../comon_src/type/utils.type";
+import { sendNotification } from "./socketCtrl";
 
 export async function newNotification(type: notificationType, fromId: number, toId: number) {
 
 	if (!type || !fromId || !toId) {
 		throw new Error("Missing required parameters");
 	}
-	const notification = await InsertDb.notification(fromId, toId, type);
+	const notification = await InsertDb.notification(fromId, toId, type) as unknown;
 	console.log(notification);
+	sendNotification(notification as notification);
+
 }
