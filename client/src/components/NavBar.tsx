945a590fa7fee93a4bdd7824db6447e11953070c
Antoine Labalette <labalette.antoine@gmail.com>
2023-08-17 21:00:15 +0200

    update navbar ui

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -15,7 +15,7 @@ import { useAuth } from "../context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import { prefixBackendUrl } from "../utils";
 import { Badge } from "@mui/material";
-import { Notifications } from "@mui/icons-material";
+import { AccountCircle, Logout, Notifications } from "@mui/icons-material";
 
 const pages = [
 	{
@@ -29,39 +29,29 @@ const pages = [
 ];
 
 function NavBar({ toggleNotification, count }: { toggleNotification: () => void; count: number }) {
-	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
 	const auth = useAuth();
 	const navigate = useNavigate();
 
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElUser(event.currentTarget);
+	const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
 	};
 
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
+	const handleCloseMenu = () => {
+		setAnchorEl(null);
 	};
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
-	const handleNavNavMenu = (url: string) => {
-		handleCloseNavMenu();
+	const handleNavMenu = (url: string) => {
+		handleCloseMenu();
 		navigate(url);
 	};
 
-	const handleNavUserMenu = (url: string) => {
-		handleCloseUserMenu();
-		navigate(url);
+	const handleNavProfile = () => {
+		navigate("/profile");
 	};
 
 	const handleLogout = async () => {
-		handleCloseUserMenu();
 		await auth.signout();
 		navigate("/");
 	};
@@ -89,20 +79,20 @@ function NavBar({ toggleNotification, count }: { toggleNotification: () => void;
 						MATCHA
 					</Typography>
 
-					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+					<Box sx={{ display: { xs: "flex", md: "none" } }}>
 						<IconButton
 							size="large"
 							aria-label="account of current user"
 							aria-controls="menu-appbar"
 							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
+							onClick={handleOpenMenu}
 							color="inherit"
 						>
 							<MenuIcon />
 						</IconButton>
 						<Menu
 							id="menu-appbar"
-							anchorEl={anchorElNav}
+							anchorEl={anchorEl}
 							anchorOrigin={{
 								vertical: "bottom",
 								horizontal: "left",
@@ -112,43 +102,25 @@ function NavBar({ toggleNotification, count }: { toggleNotification: () => void;
 								vertical: "top",
 								horizontal: "left",
 							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
+							open={Boolean(anchorEl)}
+							onClose={handleCloseMenu}
 							sx={{
 								display: { xs: "block", md: "none" },
 							}}
 						>
 							{pages.map((page) => (
-								<MenuItem key={page.label} onClick={() => handleNavNavMenu(page.url)}>
+								<MenuItem key={page.label} onClick={() => handleNavMenu(page.url)}>
 									<Typography component="a" textAlign="center">{page.label}</Typography>
 								</MenuItem>
 							))}
 						</Menu>
 					</Box>
-					<FavoriteIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href=""
-						sx={{
-							mr: 2,
-							display: { xs: "flex", md: "none" },
-							flexGrow: 1,
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
-						}}
-					>
-						MATCHA
-					</Typography>
+					<FavoriteIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1, flexGrow: 1 }} />	
 					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
 						{pages.map((page) => (
 							<Button
 								key={page.label}
-								onClick={() => handleNavNavMenu(page.url)}
+								onClick={() => handleNavMenu(page.url)}
 								sx={{ my: 2, color: "inherit", display: "block", lineHeight: "32px" }}
 							>
 								{page.label}
@@ -157,47 +129,23 @@ function NavBar({ toggleNotification, count }: { toggleNotification: () => void;
 					</Box>
 
 					{auth.user ?
-						<Box sx={{ flexGrow: 0 }}>
+						<Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", flexGrow: 0 }}>
 							<IconButton
 								size="large"
 								aria-label={`show ${count} new notifications`}
 								color="inherit"
 								onClick={toggleNotification}
-								sx={{ mr: 2 }}
 							>
 								<Badge badgeContent={count} color="error">
 									<Notifications />
 								</Badge>
 							</IconButton>
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt="Remy Sharp" src={prefixBackendUrl(auth.user.pictures[0]?.src)} />
+							<IconButton onClick={handleNavProfile} size="large" color="inherit">
+								<AccountCircle />
 							</IconButton>
-							<Menu
-								sx={{ mt: "45px" }}
-								id="menu-appbar"
-								anchorEl={anchorElUser}
-								anchorOrigin={{
-									vertical: "top",
-									horizontal: "right",
-								}}
-								keepMounted
-								transformOrigin={{
-									vertical: "top",
-									horizontal: "right",
-								}}
-								open={Boolean(anchorElUser)}
-								onClose={handleCloseUserMenu}
-							>
-								<MenuItem key='profile' onClick={() => handleNavUserMenu("/profile")}>
-									<Typography textAlign="center">Profile</Typography>
-								</MenuItem>
-								<MenuItem key='account' onClick={() => handleNavUserMenu("/account")}>
-									<Typography textAlign="center">Account</Typography>
-								</MenuItem>
-								<MenuItem key='logout' onClick={handleLogout}>
-									<Typography textAlign="center">Logout</Typography>
-								</MenuItem>
-							</Menu>
+							<IconButton onClick={handleLogout} size="large" color="inherit">
+								<Logout />
+							</IconButton>
 						</Box> :
 						<Button sx={{ my: 2, color: "inherit", display: "block", lineHeight: "32px" }} onClick={() => navigate("/login")} variant='outlined'>LOGIN</Button>
 					}
