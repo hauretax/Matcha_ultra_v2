842bb03151ba8133ba7f31a9ad94aee422a34a75
Antoine Labalette <labalette.antoine@gmail.com>
2023-08-17 22:01:47 +0200

    fix todo 11

--- client/src/components/FameFilter.tsx
+++ client/src/components/FameFilter.tsx
@@ -11,7 +11,6 @@ const FameFilter: React.FC<Props> = ({ fameRange, setFameRange }) => {
 	const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 	const buttonRef = useRef<HTMLButtonElement | null>(null);
 
-	//TODO #11: prevent age to go to 17 by pushing the Thumb 1
 	const handleAgeChange = (
 		event: Event,
 		newValue: number | number[],
@@ -27,7 +26,7 @@ const FameFilter: React.FC<Props> = ({ fameRange, setFameRange }) => {
 				setFameRange([clamped, clamped + minDistance]);
 
 			} else {
-				const clamped = Math.max(newValue[1], minDistance);
+				const clamped = Math.max(newValue[1], 18 + minDistance);
 				setFameRange([clamped - minDistance, clamped]);
 			}
 		} else {
